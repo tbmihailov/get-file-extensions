8ab901ad6ee236fc2862c3f12ec5b23d45dd9a6a
Todor B. Mihaylov <tbmihailov@gmail.com>
2013-02-22 10:35:15 +0200

    initial commit in github

---
+++ GetFileExtensions/FileFinder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace GetFileExtensions
+{
+    public class FileBrowser
+    {
+        public FileBrowser()
+        {
+        }
+
+        private static List<string> extensionList;
+        public static List<string> ExtensionList 
+        {
+            get
+            {
+                if (extensionList == null)
+                {
+                    extensionList = new List<string>();
+                }
+                return extensionList;
+            }
+            set
+            {
+                extensionList = value;
+            }
+        }
+
+        public static void ProcessDir(string sourceDir, int recursionLvl)
+        {
+
+            DirectoryInfo di = new DirectoryInfo(sourceDir);
+            var fileEntries = di.GetFiles();
+            foreach (var file in fileEntries)
+            {
+                // do something with fileName
+                ExtensionList.Add(file.Extension);
+            }
+
+            // Recurse into subdirectories of this directory.
+            DirectoryInfo[] subdirEntries = di.GetDirectories();
+            foreach (var subDir in subdirEntries)
+                // Do not iterate through reparse points
+                if ((File.GetAttributes(subDir.FullName) &
+                     FileAttributes.ReparsePoint) !=
+                         FileAttributes.ReparsePoint)
+
+                    ProcessDir(subDir.FullName, recursionLvl + 1);
+            //}
+        }
+    }
+}
---
+++ GetFileExtensions/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommandLine.Utility;
+using System.IO;
+
+namespace GetFileExtensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arguments arguments = new Arguments(args);
+            string directoryName = "";//-directoryName:"d:\Acstre\AcstreControl\Source\AcsControl\"
+            if (arguments["directoryName"] != null)
+            {
+                directoryName = arguments["directoryName"];
+            }
+            else
+            {
+                Console.WriteLine("Argument missing: \n -directoryName:\"SomeDir\"");
+                return;
+            }
+
+            directoryName = Path.GetFullPath(directoryName);
+            DirectoryInfo directory = new DirectoryInfo(directoryName);
+
+            FileBrowser.ProcessDir(directory.FullName,1);
+            var extensions = FileBrowser.ExtensionList.Distinct();
+
+
+            foreach (var ext in extensions)
+            {
+                Console.Write(string.Format("*{0} ",ext));
+            }
+            
+
+        }
+    }
+}
